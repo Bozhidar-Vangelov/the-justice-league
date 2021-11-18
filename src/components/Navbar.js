fb2bed98a984bfcca47f24b5debc8ca94a33e307
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-18 19:37:20 +0200

    Add style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from '../images/logo.svg'
 
 function Navbar() {
   return (
-    <nav className='flex justify-between items-center h-16 bg-black text-white relative shadow-sm font-mono' role='navigation'>
+    <nav className='flex justify-between items-center h-16 bg-gray-700 relative shadow-sm font-serif text-gray-300 font-bold' role='navigation'>
       <Link to='/' className='pl-8'>
         <img src={logo} alt="Logo" />
       </Link>
@@ -13,14 +13,14 @@ function Navbar() {
         </svg>
       </div>
       <div className="pr-8 md:block hidden">
-        <Link className='p-4' to='/'>Home</Link>
-        <Link className='p-4' to='/my-account'>My Account</Link>
-        <Link className='p-4' to='/create-post'>Create Post</Link>
-        <Link className='p-4' to='/guild-page'>Guild Page</Link>
-        <Link className='p-4' to='/guild-experience'>Guild Experience</Link>
-        <Link className='p-4' to='/login'>Login</Link>
-        <Link className='p-4' to='/register'>Register</Link>
-        <Link className='p-4' to='/'>Logout</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/'>Home</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/my-account'>My Account</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/create-post'>Create Post</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/guild-page'>Guild Page</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/guild-experience'>Guild Experience</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/login'>Login</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/register'>Register</Link>
+        <Link className='p-3 m-4 bg-gray-600 hover:bg-gray-800 hover:text-gray-400 rounded-lg shadow-lg' to='/'>Logout</Link>
       </div>
     </nav>
   )
