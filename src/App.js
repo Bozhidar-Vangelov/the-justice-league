f1e0948ff3bc4c01cd812d387a017f41872244ac
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-18 20:23:18 +0200

    Make dropdown responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import Dropdown from './components/Navbar/Dropdown.js';
 import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
 
   return (
     <>
