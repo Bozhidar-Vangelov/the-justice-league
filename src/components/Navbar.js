65ec045f633b61a5037a02524ba990e76899a441
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-18 17:22:34 +0200

    Add react router dom

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <Link to='/' className='pl-8'>
+            TEST
+        </Link>
+    )
+}
+
+export default Navbar
