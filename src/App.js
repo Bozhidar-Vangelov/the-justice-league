9ce4943c5e8d2b8ff5da3a6c4c55d0c24a243eab
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-24 23:01:52 +0200

    Add routes to navbar links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
+import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
+import Home from './components/Home.js';
+import MyAccount from './components/MyAccount.js';
+import CreatePost from './components/CreatePost.js';
+import GuildPage from './components/GuildPage.js';
+import GuildExperience from './components/GuildExperience.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/my-account' element={<MyAccount />} />
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/guild-page' element={<GuildPage />} />
+        <Route path='/guild-experience' element={<GuildExperience />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </>
   );
 }
