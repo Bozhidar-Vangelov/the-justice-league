e13f7c416f4737e5399e1714deb1d93bc550569d
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-25 23:30:40 +0200

    Update home background color

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='h-screen flex justify-center items-center bg-gray-700'>
+    <div className='h-screen flex justify-center items-center bg-gray-800'>
       <h1 className='text-9xl uppercase font-black text-white'>Home</h1>
     </div>
   );
