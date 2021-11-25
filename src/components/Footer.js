8eb6783abe340a00291706b47ce852b50157eb3e
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-26 00:33:00 +0200

    Add style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,15 +11,33 @@ import {
 
 function Footer() {
   return (
-    <div className='p-12 flex justify-center items-center h-16 bg-gray-700 text-white'>
+    <div className='text-2xl text-gray-300 p-12 flex justify-center items-center h-16 bg-gray-700 text-white'>
       <p>Copyright ©2021 Psychozub</p>
       <div className='m-12 grid grid-cols-3 gap-4'>
-        <FontAwesomeIcon icon={faFacebook} />
-        <FontAwesomeIcon icon={faTwitter} />
-        <FontAwesomeIcon icon={faLinkedin} />
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faYoutube} />
-        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon
+          icon={faFacebook}
+          className='hover:text-gray-900 cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faTwitter}
+          className='hover:text-gray-900 cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className='hover:text-gray-900 cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faInstagram}
+          className='hover:text-gray-900 cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faYoutube}
+          className='hover:text-gray-900 cursor-pointer'
+        />
+        <FontAwesomeIcon
+          icon={faGithub}
+          className='hover:text-gray-900 cursor-pointer'
+        />
       </div>
     </div>
   );
