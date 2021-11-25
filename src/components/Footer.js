891a647f3dc3af22b375452e34c504616c1307df
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-26 00:21:13 +0200

    Update footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+  faInstagram,
+  faYoutube,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className='p-12 flex justify-center items-center h-16 bg-gray-700 text-white'>
-      <p>Copyright © Psychozub</p>
+      <p>Copyright ©2021 Psychozub</p>
       <div className='m-12 grid grid-cols-3 gap-4'>
-        <i className='fab fa-facebook'></i>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
-        <p>TEST</p>
+        <FontAwesomeIcon icon={faFacebook} />
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faYoutube} />
+        <FontAwesomeIcon icon={faGithub} />
       </div>
     </div>
   );
