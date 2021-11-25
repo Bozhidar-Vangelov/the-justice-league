020000d27f7685f03153631557c5be3ec7c21f74
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-11-25 23:58:46 +0200

    Fix footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className='flex justify-center items-center h-16 bg-black text-white'>
+    <div className='p-12 flex justify-center items-center h-16 bg-gray-700 text-white'>
       <p>Copyright © Psychozub</p>
+      <div className='m-12 grid grid-cols-3 gap-4'>
+        <i className='fab fa-facebook'></i>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+        <p>TEST</p>
+      </div>
     </div>
   );
 }
