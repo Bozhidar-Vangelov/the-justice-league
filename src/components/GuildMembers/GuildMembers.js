84b2c78719ab29eeed451fe0d34afc8f2ce0c5b5
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-14 16:15:55 +0200

    Finalize guild members styles

--- src/components/GuildMembers/GuildMembers.js
+++ src/components/GuildMembers/GuildMembers.js
@@ -1,11 +1,23 @@
 import guildMembersStyles from './guildMembersStyles.js';
+import profileIcon from '../../images/6.png';
 
 function GuildMembers() {
   return (
     <div className={guildMembersStyles.main}>
       <section className='container mx-auto flex flex-wrap'>
         <article className={guildMembersStyles.boxContainer}>
-          <div className={guildMembersStyles.box}></div>
+          <div className={guildMembersStyles.box}>
+            <div className='border-8 border-double border-gray-900'>
+              <img src={profileIcon} alt='ProfileIcon' />
+            </div>
+            <div className={guildMembersStyles.info}>
+              <p>Summoner Name: Pesho</p>
+              <p>Level: 15</p>
+              <p>Server: EUNE</p>
+              <br />
+              <p>Favourite champion: Garen</p>
+            </div>
+          </div>
         </article>
       </section>
     </div>
--- src/components/GuildMembers/guildMembersStyles.js
+++ src/components/GuildMembers/guildMembersStyles.js
@@ -1,7 +1,17 @@
 const guildMembersStyles = {
   main: ['grid', 'min-h-screen', 'place-items-center'].join(' '),
   boxContainer: ['lg:w-1/3', 'md:w-1/2', 'w-full', 'p-4'].join(' '),
-  box: ['p-12', 'bg-gray-700', 'rounded-lg', 'shadow-lg'].join(' '),
+  box: [
+    'flex',
+    'justify-between',
+    'p-12',
+    'bg-gray-700',
+    'rounded-lg',
+    'shadow-lg',
+  ].join(' '),
+  info: ['mt-2', 'text-gray-300', 'font-serif', 'text-lg', 'font-bold'].join(
+    ' '
+  ),
 };
 
 export default guildMembersStyles;
