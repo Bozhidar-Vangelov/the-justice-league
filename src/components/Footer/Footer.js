104c0c6a0dc946354c82c56daff5a63c3e26dd4a
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-14 12:52:00 +0200

    Fix footer tag

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ import footerStyles from './footerStyles.js';
 
 function Footer() {
   return (
-    <div className={footerStyles.main}>
+    <footer className={footerStyles.main}>
       <p>Copyright ©2021 Psychozub</p>
       <div className='m-12 grid grid-cols-3 gap-4'>
         <FontAwesomeIcon icon={faFacebook} className={footerStyles.icon} />
@@ -22,7 +22,7 @@ function Footer() {
         <FontAwesomeIcon icon={faYoutube} className={footerStyles.icon} />
         <FontAwesomeIcon icon={faGithub} className={footerStyles.icon} />
       </div>
-    </div>
+    </footer>
   );
 }
 
