a823f9f91a739d019854a696316314a70afa336a
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-15 16:33:59 +0200

    Delete my account placeholders

--- src/components/MyAccount/MyAccount.js
+++ src/components/MyAccount/MyAccount.js
@@ -74,60 +74,6 @@ function MyAccount() {
             </div>
           </div>
         </article>
-        <article className={guildPostsStyles.boxContainer}>
-          <div className={guildPostsStyles.infoBox}>
-            <img src={screenshot} alt='Screenshot' />
-          </div>
-          <div className={guildPostsStyles.box} style={style}>
-            <div className={guildPostsStyles.info}>
-              <p>Author: Pesho</p>
-              <br />
-              <p>Game type: ARAM</p>
-              <br />
-              <p>
-                Description: Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Unde, quasi ducimus iure minus, numquam earum quod quia
-                non pariatur quos eius. Provident, inventore esse minus quos
-                quis quidem nam tenetur.
-              </p>
-              <br />
-              <p>Game result: Win</p>
-              <br />
-              <div className={guildPostsStyles.buttons}>
-                <button className={guildPostsStyles.upVote}>UpVote</button>
-                <p>Rating: 25</p>
-                <button className={guildPostsStyles.downVote}>DownVote</button>
-              </div>
-            </div>
-          </div>
-        </article>
-        <article className={guildPostsStyles.boxContainer}>
-          <div className={guildPostsStyles.infoBox}>
-            <img src={screenshot} alt='Screenshot' />
-          </div>
-          <div className={guildPostsStyles.box} style={style}>
-            <div className={guildPostsStyles.info}>
-              <p>Author: Pesho</p>
-              <br />
-              <p>Game type: ARAM</p>
-              <br />
-              <p>
-                Description: Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Unde, quasi ducimus iure minus, numquam earum quod quia
-                non pariatur quos eius. Provident, inventore esse minus quos
-                quis quidem nam tenetur.
-              </p>
-              <br />
-              <p>Game result: Win</p>
-              <br />
-              <div className={guildPostsStyles.buttons}>
-                <button className={guildPostsStyles.upVote}>UpVote</button>
-                <p>Rating: 25</p>
-                <button className={guildPostsStyles.downVote}>DownVote</button>
-              </div>
-            </div>
-          </div>
-        </article>
       </section>
     </div>
   );
