f961a0e6d18738729bcc03d999b24357ee643e50
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-17 15:30:34 +0200

    Create authservice

---
+++ src/services/authService.js
@@ -0,0 +1,20 @@
+function register() {
+
+}
+
+function login() {
+
+}
+
+
+function logout () {
+
+}
+
+const authService = {
+    register,
+    login,
+    logout
+};
+
+export default authService
