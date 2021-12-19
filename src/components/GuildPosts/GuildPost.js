8bb3c415b15b951a9d6d480aeb51cf4a7b5428a1
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-19 13:27:28 +0200

    Show guild posts background dynamically

--- src/components/GuildPosts/GuildPost.js
+++ src/components/GuildPosts/GuildPost.js
@@ -1,20 +1,38 @@
-import guildPostsStyles from './guildPostsStyles.js';
-import background from '../../images/7.jpg';
+import { useState } from 'react';
 
-const style = {
-  backgroundImage: `url(${background})`,
-  backgroundRepeat: 'no-repat',
-  backgroundSize: 'cover',
-  backgroundBlendMode: 'multiply',
-};
+import guildPostsStyles from './guildPostsStyles.js';
+import ARAM from '../../images/ARAM.jpg';
+import wildRift from '../../images/wildRift.jpg';
+
+let backgroundStyle = {};
 
 function GuildPost({ post }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (post.type === 'ARAM') {
+    backgroundStyle = {
+      backgroundImage: `url(${ARAM})`,
+    };
+  } else if (post.type === 'Normal' || post.type === 'Ranked') {
+    backgroundStyle = {
+      backgroundImage: `url(${wildRift})`,
+    };
+  }
+
   return (
     <article className={guildPostsStyles.boxContainer}>
       <div className={guildPostsStyles.infoBox}>
-        <img className={guildPostsStyles.image} src={post.image} alt='Screenshot' />
+        <img
+          className={guildPostsStyles.image}
+          src={post.image}
+          alt='Screenshot'
+        />
       </div>
-      <div className={guildPostsStyles.box} style={style}>
+      <div className={guildPostsStyles.box} style={backgroundStyle}>
         <div className={guildPostsStyles.info}>
           <p>Author: Pesho</p>
           <br />
@@ -28,11 +46,14 @@ function GuildPost({ post }) {
             <button className={guildPostsStyles.downVote}>DownVote</button>
           </div>
           <div className='text-center'>
-            <button className={guildPostsStyles.showDescription}>
+            <button
+              className={guildPostsStyles.showDescription}
+              onClick={toggle}
+            >
               Show description...
             </button>
           </div>
-          <p className='break-words'>{post.description}</p>
+          {isOpen ? <p className='break-words'>{post.description}</p> : ''}
         </div>
       </div>
     </article>
