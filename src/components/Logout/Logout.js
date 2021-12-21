f5dfbd814fc40569ab272415e1ed27f0004ab27b
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-12-21 23:20:21 +0200

    Add logout notification

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -3,16 +3,22 @@ import { useNavigate } from 'react-router-dom';
 
 import { useAuthContext } from '../../contexts/AuthContext';
 import authService from '../../services/authService.js';
+import {
+  useNotificationContext,
+  types,
+} from '../../contexts/NotificationContext.js';
 
 function Logout() {
   const { user, logout } = useAuthContext();
   const navigate = useNavigate();
+  const { addNotification } = useNotificationContext();
 
   useEffect(() => {
     authService.logout(user.accessToken);
     logout();
+    addNotification('Successfully logged out!', types.success);
     navigate('/');
-  }, [user.accessToken, logout, navigate]);
+  }, [user.accessToken, logout, addNotification, navigate]);
 
   return null;
 }
